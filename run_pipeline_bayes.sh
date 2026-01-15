15103243749584ca545267f6b1f44999445047e3
GonzaloPrz <gperez.ext@fi.uba.ar>
2026-01-15 11:10:04 -0300

    .

--- run_pipeline_bayes.sh
+++ run_pipeline_bayes.sh
@@ -4,7 +4,7 @@
 source "/Users/gp/miniconda3/envs/ml-env/bin/activate"
 
 # Run Python scripts with all passed arguments
-python "/Users/gp/scripts/shared/train_models_bayes.py" "$@"
-python "/Users/gp/scripts/shared/bootstrap_models_bayes.py"
-python "/Users/gp/scripts/shared/train_final_model_bayes.py"
-python "/Users/gp/scripts/shared/test_final_model_bayes.py"
+python "/Users/gp/shared_scripts/train_models_bayes.py" "$@"
+python "/Users/gp/shared_scripts/bootstrap_models_bayes.py"
+python "/Users/gp/shared_scripts/train_final_model_bayes.py"
+python "/Users/gp/shared_scripts/test_final_model_bayes.py"
