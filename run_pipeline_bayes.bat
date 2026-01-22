5181e0a6eea5c17eb9abaa98adf19b9244500a9e
GonzaloPrz <gperez.ext@fi.uba.ar>
2026-01-22 12:32:16 -0300

    Redefino el ml-env

--- run_pipeline_bayes.bat
+++ run_pipeline_bayes.bat
@@ -1,6 +1,6 @@
 @echo off
 :: Activate virtual environment
-::call "C:\ProgramData\anaconda3\condabin\activate.bat" ml-env
+call "C:\ProgramData\anaconda3\condabin\activate.bat" ml-env
 
 ::Execute Python scripts with arguments passed directly
 "C:\Users\CNC Audio\.conda\envs\ml-env\python.exe" "C:\Users\CNC Audio\gonza\shared_scripts\train_models_bayes.py" %*
